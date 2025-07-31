2e343a506cf2df3b6391b15f785d4bfa2bbe13fb
manglim-traffictech <manglim@traffictech.com>
2025-07-31 15:43:47 -0400

    Clean repository for new start

--- __tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react-native';
-import App from '../app/App';
-
-test('renders pricing calculator title', () => {
-  render(<App />);
-  const title = screen.getByText('Pricing Calculator');
-  expect(title).toBeTruthy();
-});
